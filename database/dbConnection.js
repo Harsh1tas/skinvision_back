8f674bc34a3f9d274de2890b5508bf459f14b896
Harshita Singh <159155427+Harsh1tas@users.noreply.github.com>
2024-11-04 23:13:26 +0530

    Update dbConnection.js

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -1,23 +1,15 @@
-// At the top of your app.js or dbConnection.js
-import { config } from "dotenv";
-config({ path: "./.env" }); // Make sure this matches your .env file's location
-
 import mongoose from "mongoose";
 
 export const dbConnection = () => {
   const uri = process.env.MONGO_URI;
 
-
-
   if (!uri) {
     console.error("Error: MONGO_URI is not defined in the environment variables.");
-    process.exit(1); // Exit the process if no URI is provided
+    process.exit(1);
   }
 
   mongoose.connect(uri, {
     dbName: "MERN_STACK_HOSPITAL_MANAGEMENT_SYSTEM",
-    useNewUrlParser: true,        // Recommended for stable connection parsing
-    useUnifiedTopology: true,     // Ensures use of the new connection management engine
   })
   .then(() => {
     console.log("Connected to the database successfully!");
