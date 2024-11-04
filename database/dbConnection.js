c5db881712fdfda5256d3d5075ea3f91f0d2de11
Harshita Singh <159155427+Harsh1tas@users.noreply.github.com>
2024-11-04 22:41:07 +0530

    Update dbConnection.js

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -1,14 +1,23 @@
 import mongoose from "mongoose";
 
 export const dbConnection = () => {
-  mongoose
-    .connect(process.env.MONGO_URI, {
-      dbName: "MERN_STACK_HOSPITAL_MANAGEMENT_SYSTEM",
-    })
-    .then(() => {
-      console.log("Connected to database!");
-    })
-    .catch((err) => {
-      console.log("Some error occured while connecting to database:", err);
-    });
+  const uri = process.env.MONGO_URI;
+
+  if (!uri) {
+    console.error("Error: MONGO_URI is not defined in the environment variables.");
+    process.exit(1); // Exit the process if no URI is provided
+  }
+
+  mongoose.connect(uri, {
+    dbName: "MERN_STACK_HOSPITAL_MANAGEMENT_SYSTEM",
+    useNewUrlParser: true,        // Recommended for stable connection parsing
+    useUnifiedTopology: true,     // Ensures use of the new connection management engine
+  })
+  .then(() => {
+    console.log("Connected to the database successfully!");
+  })
+  .catch((err) => {
+    console.error("An error occurred while connecting to the database:", err.message);
+    process.exit(1); // Exit the process on connection failure
+  });
 };
