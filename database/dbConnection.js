045548ce67fa7134e033ada59861c9c81bfa7252
Harshita Singh <159155427+Harsh1tas@users.noreply.github.com>
2024-11-05 21:27:21 +0530

    Update dbConnection.js

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -11,16 +11,24 @@
 
 // module.exports = connectDB;
 
+
+
+
 const mongoose = require("mongoose");
 const colors = require("colors");
 
 const dbConnection = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL);
-    console.log(`Mongodb connected ${mongoose.connection.host}`.bgGreen.white);
+    await mongoose.connect(process.env.MONGO_URL, { 
+      useNewUrlParser: true, 
+      useUnifiedTopology: true 
+    });
+    console.log(`Mongodb connected: ${mongoose.connection.host}`.bgGreen.white);
   } catch (error) {
-    console.log(`Mongodb Server Issue ${error}`.bgRed.white);
+    console.log(`Mongodb Server Issue: ${error.message}`.bgRed.white);
   }
 };
 
 module.exports = dbConnection;
+
+
