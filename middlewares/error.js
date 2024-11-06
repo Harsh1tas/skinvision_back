a53072778654689d3cb3620d5a0f07ec8c030502
Harshita Singh <159155427+Harsh1tas@users.noreply.github.com>
2024-11-06 10:48:02 +0530

    Update error.js

--- middlewares/error.js
+++ middlewares/error.js
@@ -9,23 +9,31 @@ export const errorMiddleware = (err, req, res, next) => {
   err.message = err.message || "Internal Server Error";
   err.statusCode = err.statusCode || 500;
 
+  // Handle duplicate key error
   if (err.code === 11000) {
-    const message = `Duplicate ${Object.keys(err.keyValue)} Entered`,
-      err = new ErrorHandler(message, 400);
-  }
-  if (err.name === "JsonWebTokenError") {
-    const message = `Json Web Token is invalid, Try again!`;
+    const message = `Duplicate ${Object.keys(err.keyValue)} entered.`;
     err = new ErrorHandler(message, 400);
-  }
-  if (err.name === "TokenExpiredError") {
-    const message = `Json Web Token is expired, Try again!`;
-    err = new ErrorHandler(message, 400);
-  }
-  if (err.name === "CastError") {
-    const message = `Invalid ${err.path}`,
-      err = new ErrorHandler(message, 400);
   }
 
+  // Handle invalid JWT error
+  if (err.name === "JsonWebTokenError") {
+    const message = "JSON Web Token is invalid. Try again!";
+    err = new ErrorHandler(message, 400);
+  }
+
+  // Handle expired JWT error
+  if (err.name === "TokenExpiredError") {
+    const message = "JSON Web Token has expired. Try again!";
+    err = new ErrorHandler(message, 400);
+  }
+
+  // Handle CastError (e.g., invalid ObjectId)
+  if (err.name === "CastError") {
+    const message = `Invalid ${err.path}`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  // Construct error message from mongoose validation errors if present
   const errorMessage = err.errors
     ? Object.values(err.errors)
         .map((error) => error.message)
@@ -34,7 +42,6 @@ export const errorMiddleware = (err, req, res, next) => {
 
   return res.status(err.statusCode).json({
     success: false,
-    // message: err.message,
     message: errorMessage,
   });
 };
